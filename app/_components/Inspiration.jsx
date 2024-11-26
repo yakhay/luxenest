5217d1c3b7f9a9256abca4fca1c51eb43190d596
yakhay <khayyassine450@gmail.com>
2024-11-26 19:12:42 +0100

    done9

--- app/_components/Inspiration.jsx
+++ app/_components/Inspiration.jsx
@@ -13,7 +13,7 @@ function Inspiration() {
                 <button className='w-[176px] h-[48px] bg-[#B88E2F] mt-[40px] text-white'>Explore More</button>
             </div>
             <div className='w-[404px]'>
-                <img className='w-[404px]' src="https://s3-alpha-sig.figma.com/img/f89a/66d9/4961c3801f4c07439f27b13468e5e545?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VXFiA7W7IeKXS7AvtyCdeycPs7a5lOQUvEhHPGMi3w8kr8F~a99COCsIfMYGMXkcpL5J6yOLCd0SAD-ZxnPedrfi78NEpcksTE-uIsgIlISq5Ag9ayra-pNDshr5hyJ5VzriY9ZloCtIGupu6oIDw2IWjKXyLKwRFJCF6iBHQo17PAMFKk3zfJebtUhfR13RB3U2cp5U0D8asqqey60umdZp1THIOwxrRTYLzRUrBxkzTOOwQDPQZwYitHFIJ6g0NQqsdTyIGNT-vTHgK06ohEnlnGeVE~4YfmmZzEVfLWmE3-CicJRXJdfJbWaESF4gu6U~n3I1wh3kmTlAl1m~sw__" alt="" />
+                <img className='w-[404px]' src="/data/ince.png" alt="" />
             </div>
             <div className='w-[404px]'>
                 <Swip />
--- app/_components/Swep.jsx
+++ app/_components/Swep.jsx
@@ -44,13 +44,13 @@ export default function Swip() {
 
         
           <SwiperSlide  >
-            <img className='w-[372px]' src="https://s3-alpha-sig.figma.com/img/acc4/8179/d1b18b523420e79dda1e92951ecde49b?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RwOmAn0NTP1hmZNtOrVQb9PRHAvy27JUkjKUMrekbGkkRsdxGYRn548hyeRjqaOOSa6Cl~JdjQFsENkq5OUPkiTDvW94q6QCrGvR2vD6obLnJDysyqSJCc4yJQ4dly3JRqMBEEC2JUpb3Kn5XCSrvLabEDK5Ge66OOYoUh88Vagq9qmv27m03SMulBxZ3oHUigzRIsdZ6SaGHEfASX1KuDbqeckpNWndc0IG9OkZ-MPFWHSgnQ4fmQv-KfdA8-OQZnYA~mlzoTJorX7dcLIHafZx8tn5fzbNBN0UTbE2ECtWLmooAIHa3xJIZLdLUQHQdSFN7n3bFkdtE0ASCe9J3g__" alt="" />
+            <img className='w-[372px]' src="/data/products/articl1.png" alt="" />
           </SwiperSlide>
           <SwiperSlide  >
-            <img className='w-[372px] h-full' src="https://s3-alpha-sig.figma.com/img/50f3/12dc/a7c05024ab4e27374edb12195b6559e2?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=IFKFJiYCLGtD~CKec5D5j~fka7U~laUhLRd~FjxiSmPjlQ3Xv24f38ZGCjfLtcDOInyD4my2mfoO735zfE1cc2yq7NURdDhVA9~8f-AO8~tmFmWVF6oFmOfNhG3bqR0J6T6rbkF9H8JFpW8kKWd2ol~xQpEZnNua~JjOn1PSaqn2swJs87ZfxI7qTs97HmSpUonxg-OwqUAaTl0WaZXhohY86vPg4mlJEcr8JozFRh~W63BvD9Qh1hzg7tStN2XHoFMJsU5PjbZVtrTQCJ5Xm3s3R7zNrO4o69E6IQoEPbxkH8T-qQZw5LkckkYUB9VyarVMV62j9JkyxRZA26-QuQ__" alt="" />
+            <img className='w-[372px] h-full' src="/data/products/articl4.png" alt="" />
           </SwiperSlide>
           <SwiperSlide  >
-            <img className='w-[372px]' src="https://s3-alpha-sig.figma.com/img/acc4/8179/d1b18b523420e79dda1e92951ecde49b?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RwOmAn0NTP1hmZNtOrVQb9PRHAvy27JUkjKUMrekbGkkRsdxGYRn548hyeRjqaOOSa6Cl~JdjQFsENkq5OUPkiTDvW94q6QCrGvR2vD6obLnJDysyqSJCc4yJQ4dly3JRqMBEEC2JUpb3Kn5XCSrvLabEDK5Ge66OOYoUh88Vagq9qmv27m03SMulBxZ3oHUigzRIsdZ6SaGHEfASX1KuDbqeckpNWndc0IG9OkZ-MPFWHSgnQ4fmQv-KfdA8-OQZnYA~mlzoTJorX7dcLIHafZx8tn5fzbNBN0UTbE2ECtWLmooAIHa3xJIZLdLUQHQdSFN7n3bFkdtE0ASCe9J3g__" alt="" />
+            <img className='w-[372px]' src="/data/ince2.png" alt="" />
           </SwiperSlide>
           
           <div className="absolute flex items-center justify-center cursor-pointer w-14 h-14 bg-[#b88e20] text-white rounded-full top-1/2 right-4 transform -translate-y-1/2 z-50">
